4f46747a51d05037a814986a8b8fe3d83741311a
AtharvaDev <atharva1525@gmail.com>
2021-03-03 12:49:03 +0530

    user auth

---
+++ src/firebase.js
@@ -0,0 +1,37 @@
+import firebase from "firebase"
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDLQS4K332aGyX0Ex3bF-0haVsKTDuj-4A",
+  authDomain: "getcloud.firebaseapp.com",
+  databaseURL: "https://getcloud-default-rtdb.firebaseio.com",
+  projectId: "getcloud",
+  storageBucket: "getcloud.appspot.com",
+  messagingSenderId: "481268351772",
+  appId: "1:481268351772:web:8f7a307072a91d525db0d9",
+  measurementId: "G-8SD3QDZ57G"
+};
+
+  const okapp = firebase.initializeApp(firebaseConfig)
+
+  export const auth = okapp.auth()
+
+  export default okapp
+
+
+
+// import firebase from "firebase/app"
+// import "firebase/auth"
+
+// const app = firebase.initializeApp({
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_FIREBASE_APP_ID
+// })
+
+// export const auth = app.auth()
+// export default app
